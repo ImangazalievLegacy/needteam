141feee8e1d07b2b40ab428866a5fb7655ef494d
Muhammad Imangazaliev <imangazalievm@gmail.com>
2015-08-30 11:36:38 +0300

    Service provider registered

--- config/app.php
+++ config/app.php
@@ -144,6 +144,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\BindingServiceProvider::class,
+        Modules\Api\RouteServiceProvider::class,
 
     ],
 
