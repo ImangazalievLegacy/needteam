8f16b1f15b828bd366b30e91dbd0eb761dcce5d3
Muhammad Imangazaliev <imangazalievm@gmail.com>
2015-09-06 15:36:21 +0300

    Added auth middleware for API

---
+++ modules/Api/Http/Middleware/Authenticate.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Api\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class Authenticate
+{
+    /**
+     * The Guard implementation.
+     *
+     * @var Guard
+     */
+    protected $auth;
+
+    /**
+     * Create a new filter instance.
+     *
+     * @param  Guard  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($this->auth->guest()) {
+            if ($request->ajax()) {
+                throw new AccessDeniedHttpException('Unauthorized');
+            } else {
+                // there must be authorization token
+            }
+        }
+
+        return $next($request);
+    }
+}
