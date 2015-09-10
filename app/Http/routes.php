74e2dfec01672e7e88a62d1fbc58dcd3ff7591c2
Muhammad Imangazaliev <imangazalievm@gmail.com>
2015-09-10 21:59:09 +0300

    Added routes for projects

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,37 +3,64 @@
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@getIndex']);
 
 Route::group(array('prefix' => 'account'), function(){
-	
-	Route::get('/create', [
-		'as'  => 'account.create',
-		'uses' => 'AccountController@getCreate'
-	]);
+    
+    Route::get('/create', [
+        'as'  => 'account.create',
+        'uses' => 'AccountController@getCreate'
+    ]);
 
-	Route::get('activate/{code}', array(
-		'as'   => 'account.activate',
-		'uses' => 'AccountController@getActivate'
-	))->where('code', '[a-zA-Z0-9]+');
+    Route::get('activate/{code}', array(
+        'as'   => 'account.activate',
+        'uses' => 'AccountController@getActivate'
+    ))->where('code', '[a-zA-Z0-9]+');
 
-	Route::get('/login', [
-		'as'  => 'account.login',
-		'uses' => 'AccountController@getLogin'
-	]);
+    Route::get('/login', [
+        'as'  => 'account.login',
+        'uses' => 'AccountController@getLogin'
+    ]);
 
-	Route::get('/logout', [
-		'as'  => 'account.logout',
-		'uses' => 'AccountController@getLogout'
-	]);
+    Route::get('/logout', [
+        'as'  => 'account.logout',
+        'uses' => 'AccountController@getLogout'
+    ]);
 
-	Route::group(array('middleware' => 'csrf'), function(){
+    Route::group(array('middleware' => 'csrf'), function(){
 
-		Route::post('/create', [
-			'as'  => 'account.create-post',
-			'uses' => 'AccountController@postCreate'
-		]);
+        Route::post('/create', [
+            'as'  => 'account.create-post',
+            'uses' => 'AccountController@postCreate'
+        ]);
 
-		Route::post('/login', [
-			'as'  => 'account.login-post',
-			'uses' => 'AccountController@postLogin'
-		]);
-	});
+        Route::post('/login', [
+            'as'  => 'account.login-post',
+            'uses' => 'AccountController@postLogin'
+        ]);
+    });
+});
+
+Route::get('/projects', [
+        'as'  => 'projects.show',
+        'uses' => 'ProjectController@getIndex'
+    ]);
+
+Route::group(array('prefix' => 'project'), function(){
+    
+    Route::get('/add', [
+        'as'  => 'project.add',
+        'uses' => 'ProjectController@getCreate'
+    ]);
+
+    Route::get('/{id}', array(
+        'as'   => 'project.show',
+        'uses' => 'ProjectController@getShow'
+    ))->where('code', '[a-zA-Z0-9]+');
+
+
+    Route::group(array('middleware' => 'csrf'), function(){
+
+        Route::post('/create', [
+            'as'  => 'project.create-post',
+            'uses' => 'ProjectController@postCreate'
+        ]);
+    });
 });
