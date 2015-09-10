bec9007a54f08a012d155774e481075815264420
Muhammad Imangazaliev <imangazalievm@gmail.com>
2015-09-10 22:06:35 +0300

    Tabulation corrected

--- app/Repositories/User/DbUserRepository.php
+++ app/Repositories/User/DbUserRepository.php
@@ -6,35 +6,35 @@ use App\Models\User;
 
 class DbUserRepository implements UserRepositoryInterface
 {
-	/**
-	 * Constructor
-	 * 
-	 * @param  \App\Models\User $user
-	 * @return void
-	 */
-	public function __construct(User $user)
-	{
-		return $this->users = $user;
-	}
+    /**
+     * Constructor
+     * 
+     * @param  \App\Models\User $users
+     * @return void
+     */
+    public function __construct(User $users)
+    {
+        return $this->users = $users;
+    }
 
-	/**
-	 * @param  array $data
-	 * @return bool|App\Models\User
-	 */
-	public function register($data)
-	{
-		$user = $this->users->create($data);
+    /**
+     * @param  array $data
+     * @return App\Models\User|bool
+     */
+    public function register($data)
+    {
+        $user = $this->users->create($data);
 
-		return $user->save() ? $user : false;
-	}
+        return $user->save() ? $user : false;
+    }
 
-	/**
-	 * @param  string $code
-	 * @return bool|App\Models\User
-	 */
-	public function activate($code)
-	{
-		$user = $this->users->where('hash', $code)->where('active', false);
+    /**
+     * @param  string $code
+     * @return App\Models\User|bool
+     */
+    public function activate($code)
+    {
+        $user = $this->users->where('hash', $code)->where('active', false);
 
         if ($user->count() == 0) {
             return false;
@@ -46,5 +46,14 @@ class DbUserRepository implements UserRepositoryInterface
         $user->hash   = '';
 
         return $user->save() ? $user : false;
-	}
+    }
+
+     /**
+     * @param  int $id
+     * @return App\Models\User
+     */
+    public function find($id)
+    {
+        return $this->users->find($id);
+    }
 }
--- app/Repositories/User/UserRepositoryInterface.php
+++ app/Repositories/User/UserRepositoryInterface.php
@@ -4,9 +4,21 @@ namespace App\Repositories\User;
  
 interface UserRepositoryInterface
 {
-	/**
-	 * @param  array $data
-	 * @return \App\Models\User|bool
-	 */
-	public function register($data);
+    /**
+     * @param  array $data
+     * @return \App\Models\User|bool
+     */
+    public function register($data);
+
+    /**
+     * @param  string $code
+     * @return App\Models\User|bool
+     */
+    public function activate($code);
+
+    /**
+     * @param  int $id
+     * @return App\Models\User
+     */
+    public function find($id);
 }
