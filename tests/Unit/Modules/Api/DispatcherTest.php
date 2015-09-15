a408cbec10a59f73ead2c2a7fc1568e12b5bf15b
Muhammad Imangazaliev <imangazalievm@gmail.com>
2015-09-15 14:06:51 +0300

    More tests for API dispatcher

--- tests/Unit/Modules/Api/DispatcherTest.php
+++ tests/Unit/Modules/Api/DispatcherTest.php
@@ -23,10 +23,63 @@ class DispatcherTest extends TestCase
         $this->config = $this->app->make('config');
     }
 
+    public function testHandleException()
+    {
+        $responseMock = Mockery::mock('Illuminate\Http\Response');
+        $responseMock->shouldReceive('getOriginalContent')->once()->andThrow('RuntimeException', 'Error');
+        $responseMock->shouldReceive('status')->once()->andReturn(200);
+
+        $headersMock = Mockery::mock('Symfony\Component\HttpFoundation\ResponseHeaderBag');
+        $headersMock->shouldReceive('all')->once()->andReturn([]);
+
+        $responseMock->headers = $headersMock;
+
+        $request = new Request;
+
+        $closure = function ($request) use ($responseMock) {
+            return $responseMock;
+        };
+
+        $config  = $this->config->get('api');
+        $formats = $config['formats'];
+        $mimes   = $config['mimes'];
+
+        $defaultAdapter = $config['defaultAdapter'];
+
+        $configMock = Mockery::mock('Illuminate\Contracts\Config\Repository');
+        $configMock->shouldReceive('get')->once()->with('api')->andReturn($config);
+
+        $formatter  = new Formatter();
+        $serializer = new Serializer();
+
+        $message = $config['debug'] ? 'Error' : "Internal Server Error.";
+
+        $values = [
+            ':status'   => 500,
+            ':error-message' => $message,
+        ];
+
+        $formatted  = $formatter->setFormat($formats['error'])->format($values);
+        $serialized = $serializer->serialize($formatted, $defaultAdapter);
+
+        $formatterMock = Mockery::mock('Modules\Api\Formatter');
+        $formatterMock->shouldReceive('setFormat')->once()->with($formats['error'])->andReturn($formatterMock);
+        $formatterMock->shouldReceive('format')->once()->with($values)->andReturn($formatted);
+
+        $serializerMock = Mockery::mock('Modules\Api\Serializer');
+        $serializerMock->shouldReceive('serialize')->once()->with($formatted, $defaultAdapter)->andReturn($serialized);
+
+        $appMock = Mockery::mock('Illuminate\Contracts\Foundation\Application');
+        $appMock->shouldReceive('make')->once()->with('api.formatter')->andReturn($formatterMock);
+        $appMock->shouldReceive('make')->once()->with('api.serializer')->andReturn($serializerMock);
+
+        $dispatcher = new Dispatcher($appMock, $configMock);
+
+        $response = $dispatcher->handle($request, $closure);
+    }
+
     public function testHandle()
     {
-        $this->accountService = Mockery::mock('App\Services\AccountServiceInterface');
-
         $responseMock = Mockery::mock('Illuminate\Http\Response');
         $responseMock->shouldReceive('getOriginalContent')->once()->andReturn('content');
         $responseMock->shouldReceive('status')->once()->andReturn(200);
@@ -42,13 +95,14 @@ class DispatcherTest extends TestCase
             return $responseMock;
         };
 
-        $formats = $this->getFormats();
-        $mimes   = $this->getMimes();
+        $config  = $this->config->get('api');
+        $formats = $config['formats'];
+        $mimes   = $config['mimes'];
+
+        $defaultAdapter = $config['defaultAdapter'];
 
         $configMock = Mockery::mock('Illuminate\Contracts\Config\Repository');
-        $configMock->shouldReceive('get')->once()->with('api.defaultAdapter')->andReturn('json');
-        $configMock->shouldReceive('get')->once()->with('api.formats')->andReturn($formats);
-        $configMock->shouldReceive('get')->once()->with('api.mimes')->andReturn($mimes);
+        $configMock->shouldReceive('get')->once()->with('api')->andReturn($config);
 
         $formatter  = new Formatter();
         $serializer = new Serializer();
@@ -59,14 +113,14 @@ class DispatcherTest extends TestCase
         ];
 
         $formatted  = $formatter->setFormat($formats['success'])->format($values);
-        $serialized = $serializer->serialize($formatted, 'json');
+        $serialized = $serializer->serialize($formatted, $defaultAdapter);
 
         $formatterMock = Mockery::mock('Modules\Api\Formatter');
         $formatterMock->shouldReceive('setFormat')->once()->with($formats['success'])->andReturn($formatterMock);
         $formatterMock->shouldReceive('format')->once()->with($values)->andReturn($formatted);
 
         $serializerMock = Mockery::mock('Modules\Api\Serializer');
-        $serializerMock->shouldReceive('serialize')->once()->with($formatted, 'json')->andReturn($serialized);
+        $serializerMock->shouldReceive('serialize')->once()->with($formatted, $defaultAdapter)->andReturn($serialized);
 
         $appMock = Mockery::mock('Illuminate\Contracts\Foundation\Application');
         $appMock->shouldReceive('make')->once()->with('api.formatter')->andReturn($formatterMock);
@@ -79,16 +133,6 @@ class DispatcherTest extends TestCase
         $this->assertInstanceOf('Illuminate\Http\Response', $response);
         $this->assertEquals(200, $response->status());
         $this->assertEquals($serialized, $response->content());
-    }
-
-    public function getFormats()
-    {
-        return $this->config->get('api.formats');
-    }
-
-    public function getMimes()
-    {
-        return $this->config->get('api.mimes');
     }
 
     public function tearDown()
